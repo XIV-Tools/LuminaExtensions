e403acd0d9d79fe1c69e8c1ab5ea19ab05130ecc
Yuuki-Walsh <yuuki.walsh@outlook.com>
2021-02-22 22:53:39 +1100

    Tex -> Png converter

---
+++ Converters/TexFiles/TexToPngConverter.cs
@@ -0,0 +1,39 @@
+﻿// © XIV-Tools.
+// Licensed under the MIT license.
+
+namespace LuminaExtensions.Converters.TexFiles
+{
+	using System;
+	using System.IO;
+	using System.Runtime.CompilerServices;
+	using Lumina.Data.Files;
+	using SixLabors.ImageSharp;
+	using SixLabors.ImageSharp.Formats.Png;
+	using SixLabors.ImageSharp.PixelFormats;
+
+	public class TexToPngConverter : ConverterBase<TexFile>
+	{
+		public override string Name => "Image";
+		public override string FileExtension => "png";
+
+		public override bool Convert(TexFile source, FileStream destination)
+		{
+			PngEncoder encoder = new PngEncoder();
+			encoder.BitDepth = PngBitDepth.Bit16;
+
+			////Span<byte> rawImageData = source.DataSpan.Slice(Unsafe.SizeOf<TexFile.TexHeader>());
+
+			// TODO: Split ImageData based on lods / mips
+			Image img = Image.LoadPixelData<Bgra32>(source.ImageData, source.Header.Width, source.Header.Height);
+			img.Save(destination, encoder);
+			img.Dispose();
+
+			return true;
+		}
+
+		public override bool ConvertBack(FileStream source, TexFile destination)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
