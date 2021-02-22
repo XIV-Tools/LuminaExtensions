d011a9695432acad78dc5b20009c93fa2a324daa
Yuuki-Walsh <yuuki.walsh@outlook.com>
2021-02-22 20:56:14 +1100

    Update ImcFileExtensions.cs

--- Files/ImcFileExtensions.cs
+++ Files/ImcFileExtensions.cs
@@ -3,7 +3,6 @@
 
 namespace Lumina.Data.Files
 {
-	using System;
 	using LuminaExtensions;
 
 	public static class ImcFileExtensions
@@ -13,10 +12,37 @@ namespace Lumina.Data.Files
 			return self.GetPart(slot.ToImcIndex());
 		}
 
+		public static bool HasVariant(this ImcFile self, ItemSlots slot, ushort variantId)
+		{
+			ImcFile.ImageChangeParts parts = self.GetPart(slot);
+
+			if (variantId < 0 || variantId > parts.Variants.Length)
+				return false;
+
+			return true;
+		}
+
 		public static ImcFile.ImageChangeData GetVariant(this ImcFile self, ItemSlots slot, ushort variantId)
 		{
 			ImcFile.ImageChangeParts parts = self.GetPart(slot);
 			return parts.Variants[variantId];
+		}
+
+		public static bool TryGetVariant(this ImcFile self, ItemSlots slot, ushort variantId, out ImcFile.ImageChangeData variant)
+		{
+			ImcFile.ImageChangeParts parts = self.GetPart(slot);
+			variant = default;
+
+			if (variantId < 0 || variantId > parts.Variants.Length)
+				return false;
+
+			variant = parts.Variants[variantId];
+			return true;
+		}
+
+		public static string GetMaterialKey(this ImcFile.ImageChangeData self)
+		{
+			return "v" + (self.MaterialId - 1).ToString().PadLeft(4, '0');
 		}
 	}
 }
